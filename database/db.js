3bd79200c23b3f57a2e7d44c52a92bab68acee14
Roca Maya <rocamaya631@gmail.com>
2026-08-08 13:05:11 -0600

    Corregir sintaxis de variables de entorno en db.js

--- database/db.js
+++ database/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || process.env.mysql.railway.internal,
-  user: process.env.DB_USER || process.env.root,
-  password: process.env.DB_PASSWORD || process.env.jWgLAGkmXFfYUhaoYKGGAxBUYLCUQsAk,
-  database: process.env.DB_NAME || process.env.railway, 
+  host: process.env.DB_HOST || process.env.MYSQLHOST || "mysql.railway.internal",
+  user: process.env.DB_USER || process.env.MYSQLUSER || "root",
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE || "railway", 
   port: Number(process.env.DB_PORT || process.env.MYSQLPORT) || 3306,
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
